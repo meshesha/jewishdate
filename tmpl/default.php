47a58029237986e7600d7c3e0e547b84ce3454fe
meshesha <meshesha1@gmail.com>
2016-06-03 18:51:01 +0300

    Add files via upload

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,11 +1,10 @@
 <?php
 /*
- * Module Joomla! 3.x Native Component
- * @version: 1.0
- * @copyright   Copyright (C) 2016 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- * @author: Tady Meshesha 
- * @link:http://he-il.joomla.com/jewishdate
+Module Joomla! 3.x Native Component
+@version: 1.1
+@author: Tady Meshesha 
+@license GPL2 
+@link:http://he-il.joomla.com/jewishdate
  */
  defined( '_JEXEC' ) or die( 'Restricted access' );
  
@@ -18,25 +17,8 @@ if($params->get('css'))
 
 ?>
 
-<div class="mod_jewishdate<?php echo $params->get('class_sfx') ?>" id="jewishdate_<?php echo $module->id; ?>">
-<?php
-	foreach($items as $item){
-		switch ($item)
-		{
-			case 'clock':
-				echo '<div class="jwishdate_clock" id="jewishClock_' . $params->id . '"></div>';
-				require JPATH_ROOT . '/modules/mod_jewishdate/liveclock.php';
-				break;
-			case 'day':
-				echo '<div class="jwishdate_dayname" id="jwishdate_dayname_' . $params->id . '">' . $day_name . '</div>';
-				break;
-			case 'jregorian':
-				echo '<div class="gregorian_calander" id="gregorian_calander_' . $params->id . '">' . $gregorian_date . '</div>';
-				break;
-			case 'jewish':
-				echo '<div class="jewish_calander" id="jewish_calander_' . $params->id . '">' . $jewish_date . '</div>';
-				break;
-		}
-	}
-?>
+<div class="mod_jewishdate_<?php echo $module->id; ?>" id="jewishdate_<?php echo $module->id; ?>">
+<div class="mylocaltz" id="mylocalTimezone_<?php echo $module->id; ?>" name="mylocalTimezone_<?php echo $module->id; ?>"></div>
+</br>
+
 </div>
